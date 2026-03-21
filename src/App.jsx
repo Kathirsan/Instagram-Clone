d49e79cc5dd71579b8c9fa4e0cfb02e7b8a849f1
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-03-22 05:08:19 +0530

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export default function App() {
+ function App() {
   return (
-    <div>
-      app
+    <div className="d-flex vh-100 bg-secondary">
+      <div>sidebar</div>
+      <div>feed</div>
+      <div>suggection</div>
     </div>
   )
 }
+/* w-25 w-50 w-75 w-100 */
+export default App

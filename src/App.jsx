8bddbe9f22706b17ebeb72dc003e9819c69b929e
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-03-23 04:13:11 +0530

    start feed

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Sidebar from './Sidebar'
       <div className="w-20"><Sidebar/></div>
      
       <div className="w-50 bg-info">Feed</div>
-      <div className="w-30">suggestion</div>
+      <div className="w-30">Suggestion</div>
     </div>
   )
 }

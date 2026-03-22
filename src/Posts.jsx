7baf085341d49e8ccdf11c11301dcd4e1694f7db
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-03-23 05:01:32 +0530

    add coding

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,8 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Posts() {
 
     const[posts,setPosts] = useState([]);
+
+    useEffect (()=>{
+        fetch('http://localhost:3000/posts/1').
+        then((data)=> data.json()).
+        then((data=>setPosts(data))).
+        catch(err=>console.log(err))
+    
+
+    });
 
   return (
     <div>Posts</div>

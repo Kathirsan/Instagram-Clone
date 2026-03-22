76bc69a9ba95c36eb97fae601fb72e1382dae80b
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-03-23 04:26:12 +0530

    add coding

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Feed from './Feed'
     <div className="d-flex  vh-100">
       <div className="w-20"><Sidebar/></div>
      
-      <div className="w-50 bg-info"><Feed/></div>
+      <div className="w-50 "><Feed/></div>
       <div className="w-30">Suggestion</div>
     </div>
   )

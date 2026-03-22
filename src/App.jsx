529e13a9724107f60ace9d130983e022d5fd30c3
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-03-23 04:12:24 +0530

    start feed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Sidebar from './Sidebar'
     <div className="d-flex  vh-100">
       <div className="w-20"><Sidebar/></div>
      
-      <div className="w-50 bg-info">feed</div>
+      <div className="w-50 bg-info">Feed</div>
       <div className="w-30">suggestion</div>
     </div>
   )
---
+++ src/Suggestion.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ function Suggestion() {
+  return (
+    <div>
+       
+       
+    </div>
+  )
+}
+
+export default Feed

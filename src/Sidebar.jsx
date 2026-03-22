8f6ddf1374fe55e016b670764362a66c69a67187
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-03-22 06:36:25 +0530

    update code

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import App from './App'
 
 function Sidebar() {
   return (
     <div>
-        <img src="D:\React\Instagram\src\assets\instagram-text.png" alt="" />
+        <img src="src/assets/instagram-text.png" alt="" />
     </div>
   )
 }

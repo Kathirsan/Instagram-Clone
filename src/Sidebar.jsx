735d26da6fe1f9118231e8cfef9f365d907b5103
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-03-22 07:04:23 +0530

    update code

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import App from './App'
 
+
 function Sidebar() {
   return (
     <div>
         <img src="src/assets/text.png" alt="" />
+        
     </div>
+    
   )
 }
 

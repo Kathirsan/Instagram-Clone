9260bf8ba1d7e74618e15770ff520144f0354ca7
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-03-22 07:53:43 +0530

    update code

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -7,18 +7,18 @@ function Sidebar() {
     <>
     <div>
         <img className="logo-text" src="src/assets/instagram-text.png" alt="" />
-        <div>Home</div>
-        <div>Search</div>
-        <div>Explore</div>
-        <div>Reels</div>
-        <div>Message</div>
-        <div>Notification</div>
-        <div>Create</div>
-        <div>Profile</div>
+        <div><i class="bi bi-house-door"></i>Home</div>
+        <div><i class="bi bi-search"></i>Search</div>
+        <div><i class="bi bi-compass"></i>Explore</div>
+        <div><i class="bi bi-play-circle-fill"></i>Reels</div>
+        <div><i class="bi bi-chat-dots"></i>Message</div>
+        <div><i class="bi bi-suit-heart"></i>Notification</div>
+        <div><i class="bi bi-plus-square"></i>Create</div>
+        <div><i class="bi bi-person-circle"></i>Profile</div>
     </div>
 
     <div>
-      <div>Threads</div>
+      <div><i class="bi bi-threads"></i>Threads</div>
       <div>More</div>
     </div>
     </>
